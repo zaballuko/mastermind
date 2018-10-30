588454f101940595c23a1bffc9d81c75682a7544
zaballuko <zaballuko@gmail.com>
2018-10-30 10:08:26 +0100

    Controlador claveSecreta

--- app/Http/Controllers/MastermindController.php
+++ app/Http/Controllers/MastermindController.php
@@ -9,10 +9,24 @@ class MastermindController extends Controller
     function mostarJuego(Request $request){
         /* return $request->all(); */
         $request->session()->put('nombre', $request->input("nombre"));
-        $request->session()->put('longitud', $request->input("longitud"));
+        $longitud = $request->session()->put('longitud', $request->input("longitud"));
         $request->session()->put('ballsPosibles', $request->input("ballsPosibles"));
         $request->session()->put('repetidos', $request->input("repetidos"));
         $request->session()->put('intentos', $request->input("intentos"));
+        $claveSecreta= array();
+        for ($i=0; $i < $longitud; $i++) { 
+            $num_aleatorio = rand(1,$longitud);
+            array_push($claveSecreta,$num_aleatorio);
+       }
+       $request->session()->put('claveSecreta', $claveSecreta);
+        //creas un array de clave secreta
+        return view ("juegoMastermind");
+    }
+    function jugar(Request $request){
+        for ($i=0; $i < session()->get('longitud'); $i++) { 
+            
+        }
+        return $request->all(); 
         return view ("juegoMastermind");
     }
     
--- resources/views/juegoMastermind.blade.php
+++ resources/views/juegoMastermind.blade.php
@@ -5,62 +5,39 @@
 
 @section('content')
 	<div class="content">
-	<h1>Bienvenido/a al Mastermind!</h1>
+	<h1>Bienvenido/a al Mastermind! <code>{{session()->get('nombre')}}</code></h1>
     <div class="container">
         
-	<form action="/jugar" method="post">
-            <input type="hidden" name="nombre" value="{{$nombre}}">
-			<!-- metes el nombre en un hidden para guardarlo-->
+	<form action="/jugar" method="post">      
 			@csrf
             <br>
-            <label>Introduce el codigo:</label>
-            <select name="balls">
-            <option value="8">8</option>
-            <option value="7">7</option>
-            <option value="6">6</option>
-            <option value="5">5</option>
-            <option value="4">4</option>
-            <option value="3">3</option>
-            <option value="2">2</option>
-            <option value="1">1</option>
-            </select>
-            <select name="bolas">
-            <option value="8">8</option>
-            <option value="7">7</option>
-            <option value="6">6</option>
-            <option value="5">5</option>
-            <option value="4">4</option>
-            <option value="3">3</option>
-            <option value="2">2</option>
-            <option value="1">1</option>
-            </select>
-            <select name="bolas">
-            <option value="8">8</option>
-            <option value="7">7</option>
-            <option value="6">6</option>
-            <option value="5">5</option>
-            <option value="4">4</option>
-            <option value="3">3</option>
-            <option value="2">2</option>
-            <option value="1">1</option>
-            </select>
-            <select name="bolas">
-            <option value="8">8</option>
-            <option value="7">7</option>
-            <option value="6">6</option>
-            <option value="5">5</option>
-            <option value="4">4</option>
-            <option value="3">3</option>
-            <option value="2">2</option>
-            <option value="1">1</option>
-            </select>
+             <!-- @for($i=0;$i<=session()->get('longitud');$i++){
+                <a><img src="img/ {{$clave=session()->get('clave')}} .png"></a>
+             }
+             @endfor  -->  
             <br>
-			<button type="submit"  class="btn btn-success">Comprobar</button>
+            <label>Introduce el codigo:</label>
+            @for ($i=0; $i<session()->get('longitud'); $i++)
+                <select name="selectClave{{$i}}">
+                    @for ($j=0; $j<session()->get('ballsPosibles')+1; $j++)
+                    <option value="{{$j}}">{{$j}}</option>
+                    @endfor
+                    </select>
+            @endfor
+            <br>
+			<button type="submit"  class="btn btn-success" name=comprobar>Comprobar</button>
 		</form>
         <br>
-        <h3>Jugador/a  {{$nombre}}</h3>
+        <h3>Jugador/a  <code>{{session()->get('nombre')}}</code></h3>
         <hr>
         </div>
-        <p>Intento: </p>
+        <p>Intento:
+        </p><p> / {{session()->get('intentos')}}</p>
+        <p>
+        @for($i=0;$i<session()->get('claveSecreta');$i++){
+            echo session()->get('claveSecreta')[$i];
+        }
+        @endfor
+        </p>
 	</div>
 @endsection
